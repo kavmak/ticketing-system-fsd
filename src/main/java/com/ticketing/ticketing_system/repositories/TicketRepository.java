633a3849ed32251a5812c33357b0fdd51fd8fc9f
Shailendra Singh <shailendraruhelasr@gmail.com>
2025-09-09 13:00:33 +0530

    frontend directory created

--- src/main/java/com/ticketing/ticketing_system/repositories/TicketRepository.java
+++ src/main/java/com/ticketing/ticketing_system/repositories/TicketRepository.java
@@ -2,6 +2,17 @@ package com.ticketing.ticketing_system.repositories;
 
 import com.ticketing.ticketing_system.entities.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.ticketing.ticketing_system.enums.Status;
+import java.util.List;
+import com.ticketing.ticketing_system.enums.Priority;
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
+
+    //find all tickets by status
+    List<Ticket> findByStatus(Status status);
+    
+    //find tickets by status and order them
+    List<Ticket> findByStatusOrderByCreatedAtDesc(Status status);
+
+    //find tickets by priority sorted from latest date
+    List<Ticket> findByPriorityOrderByCreatedAtDesc(Priority priority);
 }
