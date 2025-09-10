d2d2357556bdc70f600a1927d392b41ebbbd33a8
kavyas <kaveeyasharma@gmail.com>
2025-09-10 14:05:00 +0530

    Ticket table fetched

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,28 @@
+import { useState,useEffect } from "react";
+import axios from "axios";
+import Tickets from "./Tickets";
+
+
+
 function Home() {
+
+  const [tickets, setTickets] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:9090/tickets")
+      .then((response) => {
+        setTickets(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching tickets:", error);
+      });
+  }, []);
+
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>Welcome to the Home Page!</p>
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Tickets</h1>
+      <Tickets tickets={tickets} />
     </div>
   );
 }
