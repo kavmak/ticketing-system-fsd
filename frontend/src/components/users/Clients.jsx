9da04737b30c91e818cd785f684ef3373eb30901
Rithwik-03 <nadiveedhirithwik@gmail.com>
2025-09-12 11:16:43 +0530

    changes

---
+++ frontend/src/components/users/Clients.jsx
@@ -0,0 +1,12 @@
+import { useState,useEffect } from "react";
+
+
+function Clients(){
+    return (
+   <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">All Clients</h1>
+    </div>
+    );
+}
+
+export default Clients;
